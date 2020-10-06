82e912d73f8f2553594cea14d4e500fa9b4cc9cd
Noah Vogt <noah.vogt@protonmail.ch>
2020-10-06 14:32:45 +0200

    meh

--- config.h
+++ config.h
@@ -19,16 +19,17 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { ">_", "🦊", "✏", "4", "5", "6", "7", "📭", "🗑" };
 
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class      instance    title       tags mask     isfloating   isterminal noswallow   monitor */
+	{ "Gimp",     NULL,       NULL,       0,            1,          -1 },
+	{ "Firefox",  NULL,       NULL,       1 << 8,       0,          -1 },
+	{ "st",       NULL,       NULL,       NULL,         0,          -1 },
 };
 
 /* layout(s) */
